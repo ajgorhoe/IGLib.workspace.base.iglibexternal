2c56c906acc42f4783a20b68d2114da1b23c4d9a
Igor Grešovnik <gresovnik@gmail.com>
2021-11-27 19:26:30 +0100

    Updated IGLibExternal/SettingsZedGraph.bat (checkout branch updated to IGLib branch 00IGLib/21_11_26_IGLib_1_8_1_Initial)

--- IGLibExternal/SettingsZedGraph.bat
+++ IGLibExternal/SettingsZedGraph.bat
@@ -25,7 +25,8 @@ ver > nul
 
 rem Parameters for the update:
 set ModuleDirRelative=ZedGraph
-set CheckoutBranch=f7160203e2c400020046c97f61a01f470edcb4ce
+rem set CheckoutBranch=f7160203e2c400020046c97f61a01f470edcb4ce
+set CheckoutBranch=00IGLib/21_11_26_IGLib_1_8_1_Initial
 set RepositoryAddress=https://github.com/ajgorhoe/ZedGraph.git
 set RepositoryAddressSecondary=
 set RepositoryAddressLocal=d:\git_external\ZedGraph.git
