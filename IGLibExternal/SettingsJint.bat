a6a388e42b850d67a8d1887168419227c83e4b61
igor <gresovnik@gmail.com>
2022-11-06 11:24:56 +0100

    Changed branch name.

--- IGLibExternal/SettingsJint.bat
+++ IGLibExternal/SettingsJint.bat
@@ -27,7 +27,7 @@ rem Parameters for the update:
 set ModuleDirRelative=Jint
 set CheckoutBranch=00IGLib/main_IGLib
 rem   00IGLib/main_IGLib
-rem   00IGLib/22_11_06_IGLib_Initial
+rem   00IGLib/22_11_06_IGLib
 rem   00IGLib/21_12_01_IGLib_Initial
 
 set RepositoryAddress=https://github.com/ajgorhoe/jint.git
